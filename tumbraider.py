0783ed3328ae213e6647f133ce2ec51d84998ea9
dsvoid <dsvoid@gmail.com>
2017-04-14 14:07:39 -0400

    Added comments, rejigged gif saving, hid API keys (#10)

--- tumbraider.py
+++ tumbraider.py
@@ -1,3 +1,4 @@
+import keys # would-be devs: get your own keys for now and put them in a keys.py
 import argparse
 import requests
 import pytumblr
@@ -15,10 +16,15 @@ parser.add_argument("-p", "--posts", help="specify number of posts from blog to 
 parser.add_argument("-v", "--verbose", help="verbose output", action="store_true")
 args = parser.parse_args()
 
+# connect to tumblr API
 client = pytumblr.TumblrRestClient(
-# DELETE SECRETS BEFORE PUBLISHING ^^^^^^^^^^
+    keys.consumerKey,       # grab these from your keys.py file
+    keys.consumerSecret,
+    keys.OAuthKey,
+    keys.OAuthSecret
 )
 
+# set arguments
 folder = ""
 if args.folder is not None:
     folder = args.folder
@@ -35,24 +41,27 @@ if args.posts is not None and args.posts < count:
 print 'Downloading images in ' + str(count) + ' posts from ' + args.blog + '.tumblr.com...'
 
 if args.verbose and args.folder is not None:
-    print 'Saving images to ' + folder
+    print 'Saving images to ' + os.path.abspath(folder)
 
 while count > 0:
+    # request posts from tumblr API
     posts = client.posts('iksiovs',
             api_key = 'EpOrfPgSIE2v64yez9hWozCL8xLJ5eb4IbH4FX4abfMEI4Ix1x',
             offset = start,
             limit = count if count < 20 else 20)
+
+    # iterate over the results of each request
     for post in posts['posts']:
+        # look for images specifically
         if 'photos' in post:
             photoset = enumerate(post['photos'])
-            for index, photo in photoset:
-                url = photo['original_size']['url']
-                imgR = requests.get(url)
-
+            for index, photo in photoset: 
+                # format filename: timestamp, date, summary, photoset index, ext
                 filename = str(post['timestamp']) + ' ' + post['date'][:10]
 
                 if post['summary'] != '':
                     l = len(post['summary'])
+                    # strip illegal characters from summary like <>:"/\|?*
                     summary = post['summary'][:min(50, l)]
                     summary = summary.replace('<', '_')
                     summary = summary.replace('>', '_')
@@ -70,6 +79,7 @@ while count > 0:
                     sigfigs = len(str(len(post['photos'])))
                     filename = filename + ' ' + str(index+1).zfill(sigfigs)
 
+                url = photo['original_size']['url']
                 ext = url[url.rfind('.'):]
                 filename = filename + ext
 
@@ -77,14 +87,20 @@ while count > 0:
                     print filename
 
                 if folder != '':
+                    if not os.path.exists(folder):
+                        os.makedirs(folder)
                     if folder[-1] == '/':
                         folder = folder[:-1]
                     filename = folder + '/' + filename
 
-                if not os.path.exists(folder):
-                    os.makedirs(folder)
+                # download image
+                imgR = requests.get(url)
                 i = Image.open(BytesIO(imgR.content))
-                i.save(filename, save_all=True)
+                if url[-3:] != 'gif':
+                    i.save(filename)
+                else:
+                    i.save(filename, save_all=True)
+    # advance for next request
     count -= 20
     start += 20
 
