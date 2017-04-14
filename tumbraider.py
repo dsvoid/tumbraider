7f4cc3a1e59f7469168fe5daf0f222883035d3e0
dsvoid <dsvoid@gmail.com>
2017-04-13 22:18:16 -0400

    Switched to tumblr API, added better filenames

--- tumbraider.py
+++ tumbraider.py
@@ -1,5 +1,6 @@
 import argparse
 import requests
+import pytumblr
 import re
 import os
 from PIL import Image
@@ -9,75 +10,88 @@ from io import BytesIO
 parser = argparse.ArgumentParser()
 parser.add_argument("blog", help="download images from specified tumblr blog")
 parser.add_argument("-f", "--folder", help="save images to specified folder")
-parser.add_argument("-s", "--start", help="specify page from blog to start downloading images from (1 by default)", type=int)
-parser.add_argument("-p", "--pages", help="specify number of pages from blog to download images from (unlimited by default)", type=int)
+parser.add_argument("-s", "--start", help="specify post from blog to start downloading images from (0 by default)", type=int)
+parser.add_argument("-p", "--posts", help="specify number of posts from blog to download images from (unlimited by default)", type=int)
 parser.add_argument("-v", "--verbose", help="verbose output", action="store_true")
 args = parser.parse_args()
 
-print('Looking for images on ', args.blog, '.tumblr.com...', sep='')
+client = pytumblr.TumblrRestClient(
+# DELETE SECRETS BEFORE PUBLISHING ^^^^^^^^^^
+)
 
 folder = ""
 if args.folder is not None:
     folder = args.folder
 
-page = 1
+start = 0
 if args.start is not None:
-    page = args.start
+    start = args.start
 
-count = -1
-if args.pages is not None:
-    count = args.pages
+info = client.blog_info(args.blog)
+count = info['blog']['posts']
+if args.posts is not None and args.posts < count:
+    count = args.posts
 
-hasPosts = True
-while hasPosts and count != 0:
-    if args.verbose:
-        print('requesting page', page)
-    url = 'http://' + args.blog + '.tumblr.com/page/' + str(page)
-    r = requests.get(url, timeout=10)
-    if args.verbose:
-       print('on page:', page)
-    hasPosts = r.text.find('div id="post') != -1 
-    if args.verbose:
-       print('tumblr still has posts:', hasPosts)
+print 'Downloading images in ' + str(count) + ' posts from ' + args.blog + '.tumblr.com...'
 
-    # get location of images in page
-    imgIndices = [match.start() for match in re.finditer('img src="', r.text)]
-    for i in range(len(imgIndices)):
-        imgIndices[i] += 9
+if args.verbose and args.folder is not None:
+    print 'Saving images to ' + folder
 
-    # retrieve images
-    for imgIndex in imgIndices:
-        # get url of image
-        imgUrl = ''
-        i = imgIndex
-        while(r.text[i] != '"'):
-            imgUrl += r.text[i]
-            i += 1
-        # get highest possible resolution of image stored on tumblr
-        underscore = imgUrl.rfind('_')
-        period = imgUrl.rfind('.')
-        # highest resolution is already made available for audio covers
-        if imgUrl[underscore+1:period] != "cover":
-            imgUrl = imgUrl[:underscore+1] + '1280' + imgUrl[period:]
-        if args.verbose:
-            print('downloading', imgUrl[imgUrl.rfind('/')+1:])
-        # download image
-        imgR = requests.get(imgUrl)
-        filename = imgUrl[imgUrl.rfind('/')+1:]
-        if args.folder is not None:
-            filename = folder + '/' + filename
-            if not os.path.exists(folder):
-                os.makedirs(folder)
-        # save gifs differently, PIL can't download animated gifs properly
-        if imgUrl[-3:] != 'gif':
-            i = Image.open(BytesIO(imgR.content))
-            i.save(filename)
-        else:
-            with open(filename, 'wb') as f:
-                f.write(imgR.content)
+while count > 0:
+    posts = client.posts('iksiovs',
+            api_key = 'EpOrfPgSIE2v64yez9hWozCL8xLJ5eb4IbH4FX4abfMEI4Ix1x',
+            offset = start,
+            limit = count if count < 20 else 20)
+    for post in posts['posts']:
+        if 'photos' in post:
+            photoset = enumerate(post['photos'])
+            for index, photo in photoset:
+                url = photo['original_size']['url']
+                imgR = requests.get(url)
 
-    page += 1
-    if args.pages is not None:
-        count -= 1
+                filename = str(post['timestamp']) + ' ' + post['date'][:10]
 
-print('Finished downloading images from ', args.blog, '.tumblr.com', sep='')
+                if post['summary'] != '':
+                    l = len(post['summary'])
+                    summary = post['summary'][:min(50, l)]
+                    summary = summary.replace('<', '_')
+                    summary = summary.replace('>', '_')
+                    summary = summary.replace(':', '_')
+                    summary = summary.replace('"', '_')
+                    summary = summary.replace('/', '_')
+                    summary = summary.replace('\\','_')
+                    summary = summary.replace('|', '_')
+                    summary = summary.replace('?', '_')
+                    summary = summary.replace('*', '_')
+                    summary = summary.replace('\n', '')
+                    filename = filename + ' ' + summary
+
+                if len(post['photos']) > 1:
+                    sigfigs = len(str(len(post['photos'])))
+                    filename = filename + ' ' + str(index+1).zfill(sigfigs)
+
+                ext = url[url.rfind('.'):]
+                filename = filename + ext
+
+                if args.verbose:
+                    print filename
+
+                if folder != '':
+                    if folder[-1] == '/':
+                        folder = folder[:-1]
+                    filename = folder + '/' + filename
+
+                if not os.path.exists(folder):
+                    os.makedirs(folder)
+                # save gifs differently, PIL can't download animated gifs right
+                if ext != 'gif':
+                    i = Image.open(BytesIO(imgR.content))
+                    i.save(filename)
+                else:
+                    with open(filename, 'wb') as f:
+                        f.write(imgR.content)
+    count -= 20
+    start += 20
+
+
+print 'Finished downloading images from ' + args.blog + '.tumblr.com'
