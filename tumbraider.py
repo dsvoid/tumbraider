4f74a542095be43ad8a8ef869443d1f5bf01595c
dsvoid <dsvoid@gmail.com>
2017-04-21 22:30:13 -0400

    better filename formatting help

--- tumbraider.py
+++ tumbraider.py
@@ -1,5 +1,6 @@
 import keys # would-be devs: get your own keys for now and put them in a keys.py
 import argparse
+from argparse import RawTextHelpFormatter
 import requests
 import pytumblr
 import re
@@ -102,7 +103,7 @@ class tumbraider:
         l = len(post['summary'])
         replacement_dict = {
             'b' : post['blog_name'],
-            'c' : post['caption'],
+            'c' : post['caption'] if 'caption' in post else '[no caption]',
             'd' : post['date'][:-4],
             'i' : str(post['id']),
             'n' : str(post['note_count']),
@@ -228,13 +229,24 @@ class tumbraider:
 
 if __name__ == '__main__':
     # argument parsing for command-line use
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
     parser.add_argument("blog", help="download images from specified tumblr blog")
     parser.add_argument("-f", "--folder", help="save images to specified folder (program directory by default)")
-    parser.add_argument("-F", "--format", help="use specific format for filenames ($d-$b-$s by default)", type=str)
-    parser.add_argument("-p", "--posts", help="specify number of posts from blog to download images from (unlimited by default)", type=int)
-    parser.add_argument("-s", "--start", help="specify post from blog to start downloading images from (0 by default)", type=int)
-    parser.add_argument("-V", "--videos", help="also download videos hosted on tumblr", action="store_true")
+    parser.add_argument("-F", "--format", help="""use specific format for filenames ($d-$b-$s by default)
+    USEFUL CODES:
+    $b : blog name
+    $c : caption of blog post
+    $d : date and time of blog post
+    $i : id of blog post (as in [blog name].tumblr.com/post/[id of post])
+    $n : number of notes in blog post
+    $s : summary of blog post
+    $t : blog post's tags
+    $T : blog post's title
+    $u : blog post's URL""",
+    type=str)
+    parser.add_argument("-p", "--posts", help="specify number of posts from blog to download from (unlimited by default)", type=int)
+    parser.add_argument("-s", "--start", help="specify post from blog to start downloading from (0 by default)", type=int)
+    parser.add_argument("-V", "--videos", help="also download tumblr-hosted videos", action="store_true")
     parser.add_argument("-v", "--verbose", help="verbose output", action="store_true")
     args = parser.parse_args()
 
